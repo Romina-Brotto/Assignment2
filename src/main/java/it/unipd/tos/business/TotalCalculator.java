b39d4547d4c7323c341f900d9831eb7654915067
Marilena <romina.brotto@studenti.unipd.it>
2020-12-09 00:25:28 +0100

    close #1

--- src/main/java/it/unipd/tos/business/TotalCalculator.java
+++ src/main/java/it/unipd/tos/business/TotalCalculator.java
@@ -6,61 +6,25 @@ package it.unipd.tos.business;
 import java.util.List;
 import it.unipd.tos.model.MenuItem;
 import it.unipd.tos.model.User;
-
 import it.unipd.tos.business.exception.TakeAwayBillException;
 
 public class TotalCalculator implements TakeAwayBill {
     public double getOrderPrice(List < MenuItem > items, User user) throws TakeAwayBillException {
-        double tot = 0.0;   
-        int numGel= 0;
-        MenuItem GelatoMenoCostoso=null;
-        double BudGel = 0;
-       
-        
-       
+        double total = 0.0;        
+
 
         if (items == null) {
-            throw new TakeAwayBillException("lista vuota");
+            throw new TakeAwayBillException("Lista vuota");
         }
 
         if (items.contains(null)) {
-            throw new TakeAwayBillException("elemento vuoto");
-        }
-        
-        if (items.size() > 30) {
-             throw new TakeAwayBillException("30 elememti");
+            throw new TakeAwayBillException("Elemento vuoto nella lista");
         }
 
         //costo
-        for(MenuItem i: items) {
-        tot +=i.getPrice();
-        if (i.getItemType() == MenuItem.type.Gelato) {
-             numGel++;
-             BudGel += i.getPrice();
-            if ((GelatoMenoCostoso == null) || (GelatoMenoCostoso.getPrice() > i.getPrice())) {
-                GelatoMenoCostoso = i;
-            }
+        for (MenuItem i: items) { 
+            total += i.getPrice();           
         }
-            if (i.getItemType() == MenuItem.type.Budino) {
-            BudGel += i.getPrice();
-            }
-            
-        
-        
-        } 
-        if (numGel > 5) {
-            tot -= GelatoMenoCostoso.getPrice() * 0.5;
-        }
-        if (BudGel > 50) {
-        tot -= tot * 0.1;
-        }
-       
-
-        return tot;
-
-      
+        return total;     
     }
-
-
-
 }
--- src/test/java/it/unipd/tos/business/TotalCalculatorTest.java
+++ src/test/java/it/unipd/tos/business/TotalCalculatorTest.java
@@ -13,27 +13,27 @@ import it.unipd.tos.model.User;
 import it.unipd.tos.business.exception.TakeAwayBillException;
 
 public class TotalCalculatorTest {
-	
+
 	TotalCalculator calculator;
 	User user;
 	double totale;
 	List<MenuItem> list;
 	private static final double Diff = 1e-3;
-	
+
 	@Before
 	public void setup() {
 		calculator = new TotalCalculator();
 		totale = 0.00;
 		list = new ArrayList<MenuItem>();
-		user = new User("Davide","Porporati",21);
+		user = new User("Romina","Brotto",21);
 	}
-	
+
 		@Test(expected = TakeAwayBillException.class) 
 	    public void listNulltTest() throws TakeAwayBillException {
 	        list = null;
 	        totale = calculator.getOrderPrice(list,user);
 	    }
-		
+
 	@Test
 	public void sumTest() throws TakeAwayBillException {
 
@@ -46,45 +46,10 @@ public class TotalCalculatorTest {
 
         assertEquals(17.00,totale,Diff);
     }
-			
-			@Test
-			public void discount50Test() throws TakeAwayBillException {
-				list.add(new MenuItem(MenuItem.type.Gelato,"Pinguino",6.50));
-				list.add(new MenuItem(MenuItem.type.Gelato,"BananaSplit",6.50));
-				list.add(new MenuItem(MenuItem.type.Gelato,"Biancaneve",6.50));
-				list.add(new MenuItem(MenuItem.type.Gelato,"Cioccolato",5.00));
-				list.add(new MenuItem(MenuItem.type.Gelato,"Banana",3.00));
-				list.add(new MenuItem(MenuItem.type.Gelato,"FiorDiLatte",5.00));
-				list.add(new MenuItem(MenuItem.type.Gelato,"Fragola",5.00));
-				totale = calculator.getOrderPrice(list, user);
-				assertEquals(36.00,totale,Diff);
-			}
-			
-			@Test
-			public void discount10Test() throws TakeAwayBillException {
-				list.add(new MenuItem(MenuItem.type.Budino,"Cameo",30.00));
-				list.add(new MenuItem(MenuItem.type.Budino,"Biancaneve",30.00));
-				list.add(new MenuItem(MenuItem.type.Gelato,"Fragola",35.00));
-				list.add(new MenuItem(MenuItem.type.Bevanda,"Pepsi",5.00));
-				totale = calculator.getOrderPrice(list, user);
-				assertEquals(90.00,totale,Diff);
-			}
-			
+
 			@Test(expected = TakeAwayBillException.class) 
-			public void max30Test() throws TakeAwayBillException {
-				for(int i=0; i<32; i++) {
-					list.add(new MenuItem(MenuItem.type.Gelato,"Biancaneve",6.00));
-				}
-				totale = calculator.getOrderPrice(list,user);
-			}
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-}
+		    public void nullInListTest() throws TakeAwayBillException {
+				list.add(null);
+		        totale = calculator.getOrderPrice(list,user);
+		    }
+} 
